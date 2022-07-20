79df058a1a759fa97379542c8a14e6b27a1f838e
HP <HP@DESKTOP-BB1IFN8>
2022-07-20 14:08:53 +0530

    [kanchan]: Added a new project

---
+++ src/Sample1/Demo_1.java
@@ -0,0 +1,15 @@
+package Sample1;
+
+public class Demo_1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hi");
+		System.out.println("good morning");
+		System.out.println("hi rajashree");
+		
+	}
+	
+	
+
+}
